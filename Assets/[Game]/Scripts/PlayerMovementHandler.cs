fce060ee27f3728d1d89130235cd6e3b933bd3c2
bucurum <berkeucurum7@gmail.com>
2023-01-07 21:33:14 +0300

    Creating Tilemap, FirstLevel and Player Creating PlayerMovementHandler and CameraController Script Handle player movement and animations Followed player by camera

---
+++ Assets/[Game]/Scripts/PlayerMovementHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementHandler : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float jumpForce;
+    public Transform groundPoint;
+    private bool isGrounded;
+    public LayerMask groundLayer;
+    public Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        //in this code we get player movement with "ad or arrow keys"
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y); //there is 2 GetAxis method, GetAxis and GetAxisRaw, the diffrence between two is GetAxis is wait a little time and add movement to player movement, GetAxisRaw is when you press the keys the player move instantly
+    
+        //handle direction change
+        if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("speed",Mathf.Abs(rb.velocity.x));
+
+    }
+}
