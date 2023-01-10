d4bfd9c3b1a33bcb2cc699a7378e388c7b094519
bucurum <berkeucurum7@gmail.com>
2023-01-10 16:49:31 +0300

    Added dashMechanic and dash effect to player

--- Assets/[Game]/Scripts/PlayerMovementHandler.cs
+++ Assets/[Game]/Scripts/PlayerMovementHandler.cs
@@ -14,6 +14,19 @@ public class PlayerMovementHandler : MonoBehaviour
     public BulletController shotToFire;
     public Transform shotPoint;
     private bool canDoubleJump;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashTime;
+    private float dashCounter;
+    
+    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer afterImage;
+    [SerializeField] float afterImageLifeTime;
+    [SerializeField] float timeBetweenAfterImage;
+    private float afterImageCounter;
+    [SerializeField] Color afterImageColor;
+    [SerializeField] float waitAfterDashing;
+    private float dashRechargeCounter;
+
 
     void Start()
     {
@@ -21,17 +34,45 @@ public class PlayerMovementHandler : MonoBehaviour
     }
     void Update()
     {
-        //in this code we get player movement with "ad or arrow keys"
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y); //there is 2 GetAxis method, GetAxis and GetAxisRaw, the diffrence between two is GetAxis is wait a little time and add movement to player movement, GetAxisRaw is when you press the keys the player move instantly
-    
-        //handle direction change
-        if (rb.velocity.x < 0)
+        if (dashRechargeCounter > 0)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            dashRechargeCounter -= Time.deltaTime;
         }
-        else if (rb.velocity.x > 0)
+        else
         {
-            transform.localScale = Vector3.one;
+            if (Input.GetButtonDown("Fire2"))
+            {
+                dashCounter = dashTime;
+                ShowAfterImage();
+            }
+        }
+        
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            rb.velocity = new Vector2(dashSpeed * transform.localScale.x, rb.velocity.y);
+
+            afterImageCounter -= Time.deltaTime;
+            if (afterImageCounter < 0)
+            {
+                ShowAfterImage();
+            }
+            dashRechargeCounter = waitAfterDashing;
+        }
+        else
+        {
+            //in this code we set player movement with "ad or arrow keys"
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y); //there is 2 GetAxis method, GetAxis and GetAxisRaw, the diffrence between two is GetAxis is wait a little time and add movement to player movement, GetAxisRaw is when you press the keys the player move instantly
+    
+            //handle direction change
+            if (rb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (rb.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
         }
         
         isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, groundLayer);
@@ -62,4 +103,17 @@ public class PlayerMovementHandler : MonoBehaviour
         anim.SetFloat("speed",Mathf.Abs(rb.velocity.x));
 
     }
+
+    public void ShowAfterImage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position , Quaternion.identity);
+        image.sprite = spriteRenderer.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+
+        Destroy(image.gameObject, afterImageLifeTime);
+
+        afterImageCounter = timeBetweenAfterImage;
+    }
+
 }
