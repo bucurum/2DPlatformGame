1452be3ee96c81537ccd79d6855a4f7ec4a802b8
bucurum <berkeucurum7@gmail.com>
2023-01-26 01:06:42 +0300

    Added TODO

--- Assets/[Game]/Scripts/CheckPoint.cs
+++ Assets/[Game]/Scripts/CheckPoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    //TODO: whenever the player picks up the checkpoint and die, destroy the pickuped abilities
    void OnTriggerEnter2D(Collider2D other)
    {
         if (other.CompareTag("Player"))
